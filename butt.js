4c6aae1fe05f57da8ad184359937723d86bd41be
Nik Sudan <niksudan@gmail.com>
2016-03-08 02:10:05 +0000

    Make butt

---
+++ butt.js
@@ -0,0 +1,61 @@
+var Discord = require('discord.io');
+var config = require("./config");
+
+var bot = new Discord({
+    autorun: true,
+    email: config.email,
+    password: config.password,
+    username: config.username
+});
+
+function rand(length)
+{
+    return Math.floor(Math.random() * length);
+}
+
+function makeFart()
+{
+    var fart = "", maxPoo = 7;
+    for (var i = 0; i < 1 + rand(maxPoo); i++) { fart += "P"; }
+    for (var i = 0; i < 1 + rand(maxPoo); i++) { fart += "F"; }
+    for (var i = 0; i < 1 + rand(maxPoo); i++) { fart += "T"; }
+    for (var i = 0; i < 1 + rand(maxPoo); i++) { fart += "H"; }
+    return fart;
+}
+
+// Connector
+bot.on('ready', function() {
+    console.log("it's butt time");
+    bot.on('disconnected', function() {
+        setTimeout(function() {
+            bot.connect();
+        }, 5000);
+    });
+});
+
+// Listener
+bot.on('message', function (userName, userID, channelID, message, rawEvent) {
+    if (userID != bot.id) {
+    
+        // Makes itself known
+        if (message.match(/\b(butt|bum|buttock|ass|arse|fart|gas|poo|shit|crap)\b/i)) {
+            bot.sendMessage({
+                to: channelID,
+                message: "<@" + userID + ">: *" + makeFart() + "*"
+            });
+            console.log("i farted at " + userName);
+        }
+        
+        // Cloud to butt plus
+        if (message.match(/cloud/i)) {
+            var response = message.replace(/the cloud/gi, "my butt");
+            response = response.replace(/Cloud/g, "Butt");
+            response = response.replace(/cloud/gi, "butt");
+            bot.sendMessage({
+                to: channelID,
+                message: "<@" + userID + "> means to say... *\"" + response + "\"*"
+            });
+            console.log("i corrected " + userName);
+        }
+    }
+});
